2f68dd53f1544eddff0e3a6f1dbf36efc7561d32
Kun Zhang <kunzhang1988@gmail.com>
2020-04-21 17:44:53 +1000

    Handle form submission and validate form inputs

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,26 +3,48 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
     // { input, label } destructures of props of the Field
-    renderInput( {input, label} ){         
+    renderInput( {input, label} ){   
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} />
             </div>
-        ) 
-        
+        )         
+    }
+
+    onSubmit(formValues){
+        // Do not need the event.preventDefatult(), as handleSubmit can handle that.
+        // fromValues come from Fields when handleSubmit is called
+        console.log(formValues);
     }
     
-    render() {
+    render() { 
          return (
-            <form className="ui form">
+             // this.props is from redux-form
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
          );
     }   
 }
 
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+}
+
 export default reduxForm( {
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
