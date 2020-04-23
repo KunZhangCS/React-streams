62bd9851a544f2a63e4af8a8750bc9bc933ad73c
Kun Zhang <kunzhang1988@gmail.com>
2020-04-23 13:36:17 +1000

    Show error messages on Touch

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    // { input, label } destructures of props of the Field
-    renderInput( {input, label} ){   
+    // {error, touched} destructured from meta
+    renderError( {error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+
+    // { input, label, meta } destructures of props of the Field
+    // turn it to arrow function to use this keyword
+    renderInput = ({input, label, meta} ) => { 
+        // highlight the error fields 
+        const className = `field ${meta.error && meta.touched ? 'error': '' }` 
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} />
+                {/* Turn off auto complete input */}
+                <input {...input} autoComplete="off" /> 
+                {this.renderError(meta)}               
             </div>
         )         
     }
@@ -21,7 +37,8 @@ class StreamCreate extends React.Component {
     render() { 
          return (
              // this.props is from redux-form
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+             // add class 'error' to form to display error message as required by Semantic UI
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
@@ -30,6 +47,8 @@ class StreamCreate extends React.Component {
     }   
 }
 
+// Redux form will put the errors objects props to Field's component prop (this.renderInput) with the same name props
+// The validation is going to be run when the form is rendered on the screen or anytime user interacts with it
 const validate = formValues => {
     const errors = {};
 
