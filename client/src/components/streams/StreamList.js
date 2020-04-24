e3a2290a376d6334b6fee5f3d15ac984dd0341f5
Kun Zhang <kunzhang1988@gmail.com>
2020-04-24 16:58:47 +1000

    Conditionally show Edit, Delete and Create

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
     componentDidMount() {
         this.props.fetchStreams();
     }
 
+    // for Delete and Edit a stream
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {/* call renderAdmin first for Semantic UI formattion purpose */}
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -20,12 +39,25 @@ class StreamList extends React.Component {
             )
         })
     }
+
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={ { textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
     
     render() {
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         )
     }
@@ -33,7 +65,11 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     // We need an array for render method 
     //Object.values returns an array od a given objects' own enumerable property values
-    return { streams: Object.values(state.streams)};
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
