08ef4c7697f8fb8de780e44f8bf0e6395cdf9df8
Kun Zhang <kunzhang1988@gmail.com>
2020-04-24 16:15:49 +1000

    Render all streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.descripton}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    // We need an array for render method 
+    //Object.values returns an array od a given objects' own enumerable property values
+    return { streams: Object.values(state.streams)};
 }
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -17,6 +17,7 @@ export default (state = {}, action) => {
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case FETCH_STREAMS:
+            // _.mapKeys changes the array obtained from API to an object
             // ... takes out the objects from mapKeys and adds them to the big return object
             return { ...state, ..._.mapKeys(action.payload, 'id')};
         case DELETE_STREAM:
