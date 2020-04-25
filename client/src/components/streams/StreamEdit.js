11f4a218ba5143f8245c9fe908466170c60dbebc
Kun Zhang <kunzhang1988@gmail.com>
2020-04-25 21:54:45 +1000

    URL-Based Selection and Component Isolation with React Router

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+    // with React-Router, each component needs to be designed to work in isolation(fetch its own data!)
+    // So fetch the stream independently here, otherwise if the user open the edit page directly other than from the main page (streamlist), then
+    // stream will be undefined.
+    componentDidMount() {
+        //this.props from the React Router has the match prop
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        return <div>{this.props.stream.title}</div>
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
