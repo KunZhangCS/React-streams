d1061f2d64b9be8021b28d7c3149c79372d8efae
Kun Zhang <kunzhang1988@gmail.com>
2020-04-27 12:09:54 +1000

    Change put request to patch request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -45,8 +45,10 @@ export const fetchStream = (id) => async dispatch => {
     dispatch ( {type: FETCH_STREAM, payload: response.data} );
 };
 
-export const editStream = (id, formValues) => async dispatch => {    
-    const response = await streams.put(`/streams/${id}`, formValues);
+export const editStream = (id, formValues) => async dispatch => {   
+    // PUT update All properties of a record
+    // PATCH update SOME properties of a record(just need to update title and description)
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch( {type: EDIT_STREAM, payload: response.data} );
     history.push('/');
